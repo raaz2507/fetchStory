be94fef2314c32b303bd0b25e980649fa7ced440
raaz2507 <raaz2507@gmail.com>
2026-06-04 21:56:19 +0530

    add ignoreworld in transltor

---
+++ translator/engine/ignoreWoldList.js
@@ -0,0 +1,11 @@
+export const ignoreWords = new Set(['1','2','3','4','5','6','7','8','9','0','too', 'that',"spram","message",
+	"badroom", "bathroom", "bath","postion","style",  "city", "thanks", "smile","restaurant","comment",
+  "ignore","call","impresss","emotion","confuse","market","cameras","bathroom", "spycamera","camera",
+	"mom","but","please","my","this","what","was","can","hmm","just","god", "you", "ur", "how",  "she",
+	 "not", "wow", "why","know","now","will", "think","on","of","happened", "started","any","with","want", 
+	 "yes","no","up","get","all","for","have", "after","bad","done","limit","sakes","while","him","realy",
+	 "if", "would", "mostly","things","over", "simple","from","thing","lets","never", "make","join","we",
+	 "thought","your" ,"went","need","about","said","leave","seriously","at","wanted", "pick","happen", 
+	 "out", "even","age","did",  "thanx","very","still", "gf","again","talk","here","shoping","go","plz", 
+	 "dr", "am", "km","don", "dm" , "of", "mean", "its", "lady", "short", "much", "common"]);
+
--- translator/engine/transliterate.js
+++ translator/engine/transliterate.js
@@ -1,12 +1,18 @@
 const vm = require("vm");
+const fs = require("fs");
+const path = require("path");
 const axios = require("axios");
 
 const localDictionary = require("./Hindi2EnglishDic");
 
 const githubDictionaryUrl = "https://raw.githubusercontent.com/raaz2507/English2Hindi-Transliteration/main/js/Hindi2EnglishDic.js";
+const githubIgnoreWordsUrl = "https://raw.githubusercontent.com/raaz2507/English2Hindi-Transliteration/main/js/ignoreWoldList.js";
 let activeDictionary = localDictionary.Dictionary;
 let dictionarySource = "local";
 let dictionaryLoadPromise = null;
+let activeIgnoreWords = loadLocalIgnoreWords();
+let ignoreWordsSource = "local";
+let ignoreWordsLoadPromise = null;
 
 class TransliterationEngine {
 	constructor() {
@@ -17,18 +23,13 @@ class TransliterationEngine {
 
 	static async initializeDictionary() {
 		if (!dictionaryLoadPromise) {
-			dictionaryLoadPromise = loadDictionaryFromGithub()
-				.then((dictionary) => {
-					activeDictionary = dictionary;
-					dictionarySource = "github";
-					return { source: dictionarySource };
-				})
-				.catch((err) => {
-					activeDictionary = localDictionary.Dictionary;
-					dictionarySource = "local";
-					console.warn(`GitHub dictionary unavailable, using local dictionary: ${err.message}`);
-					return { source: dictionarySource, error: err.message };
-				});
+			dictionaryLoadPromise = Promise.all([
+				loadDictionaryWithFallback(),
+				loadIgnoreWordsWithFallback(),
+			]).then(() => ({
+				source: dictionarySource,
+				ignoreWordsSource,
+			}));
 		}
 
 		return dictionaryLoadPromise;
@@ -42,7 +43,7 @@ class TransliterationEngine {
 		const map = new Map();
 		let id = 0;
 
-		const protectedText = input.replace(/<[^>]+>/g, (tag) => {
+		const protectedText = input.replace(/<[^>]+>|&(?:[a-zA-Z][\w-]*|#\d+|#x[\da-fA-F]+);/g, (tag) => {
 			const key = `@@HTML_${id++}@@`;
 			map.set(key, tag);
 			return key;
@@ -91,6 +92,11 @@ class TransliterationEngine {
 
 			// skip HTML tokens
 			if (token.startsWith("@@HTML_")) {
+				output.push(token);
+				continue;
+			}
+
+			if (activeIgnoreWords.has(lower)) {
 				output.push(token);
 				continue;
 			}
@@ -180,6 +186,51 @@ async function loadDictionaryFromGithub() {
 	return dictionary;
 }
 
+async function loadDictionaryWithFallback() {
+	try {
+		activeDictionary = await loadDictionaryFromGithub();
+		dictionarySource = "github";
+	} catch (err) {
+		activeDictionary = localDictionary.Dictionary;
+		dictionarySource = "local";
+		console.warn(`GitHub dictionary unavailable, using local dictionary: ${err.message}`);
+	}
+}
+
+async function loadIgnoreWordsWithFallback() {
+	if (!ignoreWordsLoadPromise) {
+		ignoreWordsLoadPromise = loadIgnoreWordsFromGithub()
+			.then((words) => {
+				activeIgnoreWords = words;
+				ignoreWordsSource = "github";
+			})
+			.catch((err) => {
+				activeIgnoreWords = loadLocalIgnoreWords();
+				ignoreWordsSource = "local";
+				console.warn(`GitHub ignore word list unavailable, using local list: ${err.message}`);
+			});
+	}
+
+	return ignoreWordsLoadPromise;
+}
+
+async function loadIgnoreWordsFromGithub() {
+	const response = await axios.get(githubIgnoreWordsUrl, {
+		responseType: "text",
+		timeout: 15000,
+		headers: {
+			"User-Agent": "fetchStory-transliterator",
+		},
+	});
+	const words = evaluateIgnoreWordsModule(response.data, "ignoreWoldList.github.js");
+
+	if (!(words instanceof Set) && !Array.isArray(words)) {
+		throw new Error("GitHub ignore word list did not export ignoreWords");
+	}
+
+	return normalizeIgnoreWords(words);
+}
+
 function evaluateDictionaryModule(source) {
 	const runnableSource = source
 		.replace(/^\s*export\s+const\s+Dictionary\s*=/m, "const Dictionary =")
@@ -199,6 +250,58 @@ function evaluateDictionaryModule(source) {
 	});
 
 	return sandbox.module.exports.Dictionary || sandbox.Dictionary;
+}
+
+function loadLocalIgnoreWords() {
+	const ignorePath = path.join(__dirname, "ignoreWoldList.js");
+
+	try {
+		const source = fs.readFileSync(ignorePath, "utf8");
+		return normalizeIgnoreWords(
+			evaluateIgnoreWordsModule(source, "ignoreWoldList.js"),
+		);
+	} catch (err) {
+		console.warn(`Ignore word list unavailable: ${err.message}`);
+		return new Set();
+	}
+}
+
+function evaluateIgnoreWordsModule(source, filename) {
+	const runnableSource = source
+		.replace(/^\s*export\s+const\s+ignoreWords\s*=/m, "const ignoreWords =")
+		.replace(/^\s*export\s*\{\s*ignoreWords\s*\}\s*;?\s*$/m, "")
+		.replace(/module\.exports\s*=\s*\{\s*ignoreWords\s*\}\s*;?\s*$/m, "")
+		.concat(
+			"\nif (typeof ignoreWords !== 'undefined') module.exports = { ignoreWords };\n",
+		);
+	const sandbox = {
+		module: { exports: {} },
+		exports: {},
+		Set,
+	};
+
+	sandbox.exports = sandbox.module.exports;
+
+	vm.runInNewContext(runnableSource, sandbox, {
+		filename,
+		timeout: 1000,
+	});
+
+	return sandbox.module.exports.ignoreWords || sandbox.ignoreWords;
+}
+
+function normalizeIgnoreWords(words) {
+	const values = words instanceof Set ? Array.from(words) : words;
+
+	if (!Array.isArray(values)) {
+		return new Set();
+	}
+
+	return new Set(
+		values
+			.map((word) => String(word).trim().toLowerCase())
+			.filter(Boolean),
+	);
 }
 
 module.exports = TransliterationEngine;
